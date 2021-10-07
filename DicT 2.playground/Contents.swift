0efcd302e96256c310f02a1d43f1416c8a37fa31
Hind Aljehani <Hind.Aljehani.has.s@tuwaiq.edu.sa>
2021-10-07 11:55:27 +0300

    Done it

---
+++ DicT 2.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+
+var Mohammed = [ "Hail": 3 , " Medina" : 4 , "Tabuk": 1]
+var Faris = [ "Riyadh": 5 , " Dmmam" : 2 , "Jizan": 3 , " Qasim" : 10]
+
+for (key, Value) in Mohammed {
+    
+    print (" Mohammed visited ,\(key),\(Value), Times")
+}
+
+
+for (key, Value) in Faris {
+    
+    print (" Faris visited ,\(key),\(Value), Times")
+}
