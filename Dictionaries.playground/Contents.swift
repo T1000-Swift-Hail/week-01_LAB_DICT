f576cd3091c2beb5110f018bcb765397070f6cd6
Monafh <monafh@MacBook-Pro-alkhas-b-Monafh.local>
2021-10-07 10:24:42 +0300

    DicT SoLve..

---
+++ Dictionaries.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+var Mohammed = ["Hail": 3, "Medina": 4 , "Tabuk": 1  ]
+var Faris = ["Riyadh": 5, "Dammam" : 2 , "Jizan": 3, "Qasim": 10 ]
+
+
+for (key, value) in Mohammed{
+  
+    print("Mohammed visited,\(key), \(value) times")
+    
+}
+print ("")
+
+for (key, value) in Faris{
+    print("Faris visited,\(key), \(value) times")
+    
+}
+
+// print("Mohammed visited ,\(forKey : Int) times")
