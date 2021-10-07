8f7148404efe4931cd1f491579bd327c44e92421
Mohammed Abdullah <mona@MacBook-Pro-alkhas-b-mohammed.local>
2021-10-07 11:09:54 +0300

    solv the lab

---
+++ lab dict.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+var mohammed=["hail": 3 , "madina": 4  , "tabuk": 1 ]
+var faris=["riyadh": 5 , "dammam": 2 , "jizan": 3 , "Qasim": 10 ]
+for (key,value) in mohammed {
+    print("mohammed visited\(key)\(value)times")
+}
+for (key,value) in faris {
+    print("faris visited\(key)\(value)times")
+    
+}
+
+
