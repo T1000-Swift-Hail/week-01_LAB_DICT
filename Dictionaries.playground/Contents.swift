172ec64e5285adcef1859b5661234f29ca78a05f
Majed Alshammari <admin@Majeds-MacBook-Air.local>
2021-10-07 10:51:37 +0300

    Lab dict done

---
+++ Dictionaries.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+import Foundation
+
+let Mohammed = ["Hail":3,"Medina":4,"Tabuk":1]
+let  Faris = ["Riyadh":5,"Dammam":2,"Jizan":3]
+
+
+for (index, dicM) in Mohammed {
+    print("Mohammed visited \(index) \(dicM) times")
+         
+}
+print()
+
+for (index, dicF) in Faris {
+    print("Faris visited \(index) \(dicF) times")
+}
