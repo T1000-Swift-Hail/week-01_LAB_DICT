38ac2ec93c3b90b47bc66f3d049beccc18c6c988
user <user@MacBook-Air-.local>
2021-10-07 10:53:41 +0300

    lab dic is done

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+import Darwin
+
+let Mohammed = ["Hail": 3, "Medina": 4, "Tabuk": 1]
+let Faris = ["Riyadh": 5, "Dammam": 2, "Jizan": 3, "Qasim": 10]
+for (index, dicM) in Mohammed {
+    print("Mohammed visited \(index)\(dicM)")
+     
+}
+for (index, dicF) in Faris {
+    print("Faris visited \(index)\(dicF)")
+    
+
+    
+}
+
+
+
