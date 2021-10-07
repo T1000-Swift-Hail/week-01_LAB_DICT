428c3d5bab7de564916fa3f56a371e0a60dbd259
طلال عبيدالله دعيع القلادي <talalalglaadi@MacBook-Air-tlal.local>
2021-10-07 11:14:02 +0300

    EX Dict Done

---
+++ EX DICT.playground/Contents.swift
@@ -0,0 +1,14 @@
+var Mohammed = ["Hail": 3, "Medina": 4, "Tabuk":1]
+var Faris = ["Riyadh ": 5, "Dammam ": 2, "Jizan ": 3,"Qasim": 10]
+
+for (cities , visited) in Mohammed
+{print("Mohammed visited \(cities):\(visited) times")
+    
+}
+    
+for (cities , visited) in Faris
+{print("Faris visited \(cities):\(visited)times")
+    
+}
+
+
