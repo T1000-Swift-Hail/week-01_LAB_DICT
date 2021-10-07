77d00fb6adec5ce7976dd0d0e5577e944de125de
hesah <hesah.raja.has.s@tuwaiq.edu.sa>
2021-10-07 11:06:57 +0300

    Lab DICI

---
+++ Lab.DICT.playground/Contents.swift
@@ -0,0 +1,19 @@
+import Cocoa
+
+let Mohammed=["Hail" :3 ,"Madinah" : 4 ,"tabuk" : 1]
+
+let Faris=["Riyadh" :5 ,"Dammam" : 2,"Jizan" : 3 ,"Qasim" : 10]
+
+for (city , time) in Mohammed  {
+    
+    print("mohammed visited \(city) \(time) time")
+
+    
+}
+
+for (city , time) in Faris  {
+ 
+
+    print("Faris visited \(city) \(time) time")
+    
+}
