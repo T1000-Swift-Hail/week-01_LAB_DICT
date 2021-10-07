069fa499140157dab8cf5fc7d9896280556928da
Hind Alharbi <Hind.Alharbi.has.s@tuwaiq.edu.sa>
2021-10-07 10:36:57 +0300

    week-01_LAB_DICT

--- week-01_LAB_DICT.playground/Contents.swift
+++ week-01_LAB_DICT.playground/Contents.swift
@@ -1,3 +1,16 @@
 import UIKit
 
-var greeting = "Hello, playground"
+var mohammed = ["Hail": 3, "Medina": 4, "Tabuk":1 ]
+
+var faris = ["Riyadh": 5, "Dammam": 2, "Jizan": 3, "Qasim": 10]
+
+for (key,value) in mohammed {
+    
+      print(" Mohammed visited \(key): \(value)times ")
+    }
+  
+for (key,value) in faris {
+    
+      print(" faris visited \(key): \(value)times ")
+    }
+  
