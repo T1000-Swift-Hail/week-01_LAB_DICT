255ad60014659744ad7288d47da800c33ff33085
Mastorah <mas@Mastorahs-MacBook-Air.local>
2021-10-07 10:52:21 +0300

    finish

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+import Foundation
+
+var Mohammed = ["Hail": 3 , "Medina": 4 , "Tabuk": 1]
+for (name,number) in Mohammed {
+    print("Mohammed visited \(name)\(number)times")}
+
+
+
+var Faris = ["Riyadh": 5 , "Dammam": 2 , "Jizan": 3, "Qasim": 10]
+for (name,number) in Faris {
+print("Faris visited \(name)\(number)times")}
+
+
