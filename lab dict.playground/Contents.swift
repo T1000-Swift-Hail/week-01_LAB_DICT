4c507ea160107d1ade1f68b21f63910339e12dfa
ahmad1aa <Ahmed.Atallah.has.s@tuwaiq.edu.sa>
2021-10-07 11:08:20 +0300

    done lab

---
+++ lab dict.playground/Contents.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+var Mohammad = ["hail": 3,"medina":4,"tabuk":1]
+var faris = ["riyadh":5,"dammam":2,"jizan":3,"qassim":10]
+
+for (key,value) in Mohammad {
+    
+    print("Mohammad visited \(key) \(value) times")
+    
+    
+}
+
+
+
+
+for (key,value) in faris {
+    
+   
+    print("faris visited \(key) \(value) times")
+    
+    //print("faris visited\(visited)times")
+
+
+
+}
+    
+    
+    
+    
+    
+//print(5, terminator: "riyadh")
