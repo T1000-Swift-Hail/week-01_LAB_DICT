0d1e8a26f897a94b5b7600c16b65acc9a1af344a
Seham الشطنان <sehamalshtnan@Sehams-MacBook-Air.local>
2021-10-07 11:07:13 +0300

    lab done

---
+++ Dic lab.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+import Darwin
+
+var mohammed = ["hail":3,"medina":4, "Tabuk":1]
+var Faris = ["Riyadh":5,"Dmmam":2,"jizan":3,"Qasim":10]
+
+for (key, value) in mohammed {
+        print(" mohammed visited ,\(key),\(value) times")
+}
+
+for (key,value)in Faris {
+        print(" Faris visited ,\(key),\(value) times")
+}
+
+
