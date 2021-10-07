e2ed40b58f30baeca03dd84154f508319dfe5dba
AsmaRashed1 <Asma.Rashed.has.s@tuwaiq.edu.sa>
2021-10-07 10:47:16 +0300

    Done Lab DICT

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+var Mohammed = ["Hail": 3 ,"Medina": 4 ,"Tabuk": 1  ]
+
+for (key ,value) in Mohammed{
+    
+    
+    print ("Mohammed visited \(key) \(value)times")
+}
+    
+var Faris = ["Riyadh": 5 , "Dammam": 2 , "Jizan": 3 , "Qasim": 10 ]
+
+for (x ,y )  in Faris{
+        
+    
+        print ( "Faris visited \(x) \(y)times")
+}
