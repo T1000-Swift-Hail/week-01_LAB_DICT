16c57e304e9653064fec942ad29ef0ea6686b6f3
Anas Hamad <anashamad@MacBook-Air-Anas.local>
2021-10-07 10:32:44 +0300

    lab is done

---
+++ dict Lab.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+import Foundation
+
+var mohammed = [ "Hail":3, "Medina":4, "Tabuk":1]
+var Faris = ["Riyadh":5, "dammam":2, "Jizan":3,"Qasim":10]
+
+
+for (key, Value) in mohammed {
+    print("Mohammed visited \(key),\(Value) times")
+}
+for (key, value) in Faris{
+    print("Faris visited \(key),\(value) times")
+}
+//print ("Mohammed visited \(mohammed)")
+//print("Faris visited \(Faris)")
+
