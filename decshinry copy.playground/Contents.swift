22dbf716070156dff876e3689b7efc48768fd8f4
hanaalmutlak <Hana.Khamis.has.s@tuwaiq.edu.sa>
2021-10-07 11:37:11 +0300

    Dic lab

---
+++ decshinry copy.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+var mohammed = ["Hail": 3, "Medina": 4, "Tabuk": 1]
+  var Faris = ["Riyadh": 5 , "Dammam": 2 , "Jizan": 3 , "Qasim": 10 ]
+
+for (key, value) in mohammed  {
+    print("mohammed visited \(key) \(value) times")
+}
+for (key, value) in Faris {
+    print(" Faris visited \(key) \(value) times ")
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
