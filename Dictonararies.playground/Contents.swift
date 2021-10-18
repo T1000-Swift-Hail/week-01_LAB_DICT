2ed682c852717c97788199b858f12e4991af5324
Munira abdullah <muniraabdullah@MacBook-alkhas-b-Munira.local>
2021-10-18 09:11:00 +0300

    Done New

---
+++ Dictonararies.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+import Darwin
+
+var Mohammed = ["Hail" : 3, "Medina" : 4, "Tabuk" : 1]
+var Faris = ["Riyadh" : 5, "Dammam" : 2, "Jizan" : 3, "Qasim" : 10]
+for (key, value) in Mohammed {
+    print(" Mohammed visited \(key) \(value) times ")
+}
+for (key, value) in Faris {
+print(" Faris visited \(key) \(value) times ")
+}
